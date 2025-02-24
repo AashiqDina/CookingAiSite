d3038a99444a994c69a62db94fbdcd3bd54cada0
AashiqDina <98742552+AashiqDina@users.noreply.github.com>
2025-02-24 16:07:45 +0000

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+
+
+export default function TheHeader(){
+    return(
+        <header>
+            <img src="/images/Chef-Hat.png" class/>
+            <h1 class="HeaderTitle">QuickCook</h1>
+        </header>
+    )
+}
